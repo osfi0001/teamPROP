15c4d7ca4b5dba3cd3266975c9f2e24cedc715f2
osfi0001 <osfi0001@stud.kea.dk>
2024-09-09 21:51:48 +0200

    lavet en base layout på det meste

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// TO KNOW THAT THE SITE LOADS IT SHOULD SHOW "sidenVises" IN THE CONSOLE OTHERWISE NOTHING WILL WORK (THIS CAN BE DELETED LATER)
+window.addEventListener("load", sidenVises);
+function sidenVises() {
+  console.log("sidenVises");
+}
+// CONTINUE THE JS NEEDED BELOW
